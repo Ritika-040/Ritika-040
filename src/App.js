06c3aeedcd3e27eb92c68808004d1d252bb1649b
Ritika <ritikasharmaa.23@gmail.com>
2021-12-23 17:28:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import images from './images.json'
+import ImageContainer from './Components/image-container'
+import "./App.css";function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        {images.map(res => {
+          return (
+            <div key={res.id} className="wrapper">
+              <ImageContainer
+                src={res.urls.regular}
+                thumb={res.urls.thumb}
+                height={res.height}
+                width={res.width}
+                alt={res.alt_description}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
-
-export default App;
+}export default App;
